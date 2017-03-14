ce9309d8c62b3dcf22a5a3aa687c32dcbd45df5e
Courtney Miller <courtneymiller919@gmail.com>
2017-03-13 18:57:13 -0600

    angular assignment 9 updated

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -82,7 +82,7 @@
                  scope.trackThumb = function() {
                     $document.bind('mousemove.thumb', function(event) {
                         var percent = calculatePercent(seekBar, event);
-                         scope.$apply(function() {
+                          scope.$apply(function() {
                             scope.value = percent * scope.max;
                           });
                     });
