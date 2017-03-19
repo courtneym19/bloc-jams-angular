c0403a9389efc9f6ad55d93b4e2836996f672cb1
Courtney Miller <courtneymiller919@gmail.com>
2017-03-19 14:06:17 -0600

    angular asst 11

--- dist/scripts/filters/timecode.js
+++ dist/scripts/filters/timecode.js
@@ -7,18 +7,8 @@
                 return '-:--';
              }
              
-             var wholeSeconds = Math.floor(seconds);
-             var minutes = Math.floor(wholeSeconds / 60);
-             var remainingSeconds = wholeSeconds % 60;
- 
-             var output = minutes + ':';
- 
-             if (remainingSeconds < 10) {
-                 output += '0';   
-             }
- 
-             output += remainingSeconds;
-             return output;
+             var timer = buzz.toTimer(seconds);
+             return timer;
          };
      }
  
